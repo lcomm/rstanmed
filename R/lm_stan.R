42221ac12b0cec9b66d1d7468482227a3fe45ded
Leah Comment <leah.comment@gmail.com>
2018-01-25 13:51:30 -0500

    Initial build for shell package

---
+++ R/lm_stan.R
@@ -0,0 +1,10 @@
+#' Bayesian linear regression with Stan
+#'
+#' @param x vector of inputs
+#' @param y vector of outputs
+#' @return an object of class `stanfit` returned by `rstan::sampling`
+#' @export
+lm_stan = function(x, y) {
+  out = rstan::sampling(stanmodels$lm, data=list(x=x, y=y, N=length(y)))
+  return(out)
+}
---
+++ R/rstanmed-package.R
@@ -0,0 +1,15 @@
+#' Bayesian Mediation in Stan
+#'
+#' @docType package
+#' @name rstanmed-package
+#' @aliases rstanmed
+#' @useDynLib rstanmed, .registration = TRUE
+#'
+#' @import methods
+#' @import rstan 
+#' @import stats
+#' @import Rcpp
+#' @import rstantools
+#'
+#' @description stuff
+NULL
